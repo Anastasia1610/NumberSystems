26c249576377d99fb20df3995deaa816a4b57f1b
пк <@DESKTOP-70U3VC3>
2023-01-06 00:28:46 +0200

    Added a transfer of Decimal number to Out System

--- NumberSystems/MainWindow.xaml.cs
+++ NumberSystems/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace NumberSystems
         private int OutSystem;
         private string Number;
 
-        
+
 
         private void InButton_Click(object sender, RoutedEventArgs e)
         {
@@ -41,7 +41,7 @@ namespace NumberSystems
                 if (item is Button)
                 {
                     ((Button)item).Background = Brushes.LightGray; //Все кнопки StackPanel "In" становятся серыми
-                    if(item == btn)
+                    if (item == btn)
                         InSystem = system + 2;
                     system++;
                 }
@@ -67,6 +67,48 @@ namespace NumberSystems
         private void TransferButton_Click(object sender, RoutedEventArgs e)
         {
             Number = InField.Text;
+
+            if (InSystem != 10)  //Перевод входящего числа в десятичную систему
+            {
+                string[] arr = new string[Number.Length];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = Number[i].ToString();
+                    if (arr[i] == "A")
+                        arr[i] = "10";
+                    else
+                    if (arr[i] == "B")
+                        arr[i] = "11";
+                    else if (arr[i] == "C")
+                        arr[i] = "12";
+                    else if (arr[i] == "D")
+                        arr[i] = "13";
+                    else if (arr[i] == "E")
+                        arr[i] = "14";
+                    else if (arr[i] == "F")
+                        arr[i] = "15";    
+                }
+
+                int sum = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += int.Parse(arr[i]) * (int)(Math.Pow(InSystem, arr.Length - i - 1));
+                }
+                Number = sum.ToString();
+            }
+
+            //Перевод числа из десятичной систему в исходящую
+            string OutNumber = "";
+            do
+            {
+                OutNumber += int.Parse(Number) % OutSystem;
+                Number = (int.Parse(Number) / OutSystem).ToString();
+            } while (int.Parse(Number) > OutSystem);
+            OutNumber += int.Parse(Number);
+            char[] array = OutNumber.ToCharArray();
+            Array.Reverse(array);
+            OutNumber = new string(array);
+            OutField.Text = OutNumber;
         }
     }
 }
